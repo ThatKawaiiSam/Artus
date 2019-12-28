b2550eda11261cb13a7dfdc642210fc6dd8c356a
ThatKawaiiSam <sam.sergi@outlook.com>
2019-12-29 09:17:13 +1100

    Borked the repo.

--- src/main/java/io/github/thatkawaiisam/plugintemplate/shared/messsagequeue/impl/RabbitMQMessageQueueImpl.java
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-package io.github.thatkawaiisam.plugintemplate.shared.messsagequeue.impl;
-=======
-package io.github.plugintemplate.shared.messsagequeue.impl;
->>>>>>> master
-
-public class RabbitMQMessageQueueImpl {
-}
